31302984405bfa2bb37a1214fa7df445c3941054
Ryan Raaum <github@raaum.org>
2025-02-05 11:28:54 -0500

    add example data and documentation

---
+++ R/data.R
@@ -0,0 +1,63 @@
+#' A single journal article from crossref
+#'
+#' An R list with the crossref data for a single journal article.
+#' It is included here for trial and testing purposes to avoid repeatedly hitting
+#' crossref servers for no good reason.
+#' It could be accessed directly from crossref using
+#' \preformatted{
+#' rcrossref::cr_cn(dois = "10.1371/journal.pcbi.1009061",
+#'                  format = "citeproc-json")
+#' }
+#'
+#' @format
+#' A list with 43 elements.
+#'
+#' @source <https://www.crossref.org>
+"cr_journal_article"
+
+
+#' A single journal article from openalex
+#'
+#' An R list with the openalex data for a single journal article.
+#' It is included here for trial and testing purposes to avoid repeatedly hitting
+#' openalex servers for no good reason.
+#' It could be accessed directly using
+#' \preformatted{
+#' openalexR::oa_query(entity = "works",
+#'                     doi = "10.1371/journal.pcbi.1009061") |>
+#'            openalexR::oa_request()
+#' }
+#'
+#' @format
+#' A list with 50 elements.
+#'
+#' @source <https://www.openalex.org>
+"oa_journal_article"
+
+
+#' A single journal article from pubmed
+#'
+#' XML text with the pubmed data for a single journal article.
+#' It is included here for trial and testing purposes to avoid repeatedly hitting
+#' pubmed servers for no good reason.
+#' It could be accessed directly using
+#' \preformatted{
+#' this_res <- rentrez::entrez_search("pubmed",
+#'                                    term="10.1371/journal.pcbi.1009061")
+#' this_pmid <- this_res$ids[1]
+#' this_record <- rentrez::entrez_fetch("pubmed",
+#'                                      id=this_pmid,
+#'                                      rettype="xml")
+#' }
+#' Note that we cannot save processed XML because the internal R representation
+#' does not permit it, so this example data needs to be parsed before use:
+#' \preformatted{
+#' pm_journal_article <- xml2::read_xml(pm_journal_article_xmltext)
+#' }
+#'
+#' @format
+#' A string of XML data.
+#'
+#' @source <https://pubmed.ncbi.nlm.nih.gov/>
+"pm_journal_article_xmltext"
+
---
+++ data-raw/package_data.R
@@ -0,0 +1,13 @@
+## code to prepare `package_data` goes here
+
+cr_journal_article <- readRDS("data-raw/crossref_basic_journal_article.rda")
+usethis::use_data(cr_journal_article, overwrite = TRUE)
+
+oa_journal_article <- readRDS("data-raw/openalex_basic_journal_article.rda")
+usethis::use_data(oa_journal_article, overwrite = TRUE)
+
+pm_journal_article_file <- "data-raw/pubmed_basic_journal_article.xml"
+pm_journal_article_file_size <- file.info(pm_journal_article_file)$size
+pm_journal_article_xmltext <- readChar(pm_journal_article_file,
+                                       pm_journal_article_file_size)
+usethis::use_data(pm_journal_article_xmltext, overwrite = TRUE)
